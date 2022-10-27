82b85d5e6bd1e196adc1c3478a84dc9174dd87f7
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-10-26 20:16:27 -0400

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import * as dotenv from 'dotenv';
+import {router} from './routes/public';
+
+// config
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes
+app.use(router);
+
+// server
+const server = app.listen(process.env.SERVER_PORT, ()=> {
+    console.log(`server running at port: ${process.env.SERVER_PORT}`);
+});
+
+process.on('SIGINT', () => {
+    server.close();
+    console.log('server closed');
+});
